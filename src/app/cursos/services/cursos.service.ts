0d7799b8215ee2f2259ad4a2f18761da6e62b18e
Leonardo França <lsfranca@revizia.com.br>
2023-01-03 11:21:01 -0300

    Adicionando proxy para api

--- src/app/cursos/services/cursos.service.ts
+++ src/app/cursos/services/cursos.service.ts
@@ -8,7 +8,7 @@ import { delay, first, Observable, tap } from "rxjs";
 })
 export class CursosService {
 
-  private readonly API = '/assets/cursos.json';
+  private readonly API = 'api/cursos';
 
   constructor(private httpClient: HttpClient) {
   }
